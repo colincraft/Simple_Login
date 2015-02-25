d75d0ce527a9d33f7b98e5572370066eae912e80
Colin Craft <colin@colincraft.com>
2015-02-25 07:59:28 -0800

    All major functionality implemented

---
+++ app/controllers/access_controller.rb
@@ -0,0 +1,66 @@
+class AccessController < ApplicationController
+  before_action :prevent_login_signup, only: [:signup, :login]
+
+  def signup
+    @user = User.new
+  end
+
+  def login
+  end
+
+  def home
+  end
+
+  def logout
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to home_path
+    else
+      render :signup
+    end
+  end
+
+  def attempt_login
+    if params[:username].present? && params[:password].present?
+      found_user = User.where(username: params[:username]).first
+      if found_user
+        authorized_user = found_user.authenticate(params[:password])
+        if authorized_user
+          redirect_to home_path
+        else 
+          redirect_to login_path
+        end
+      else
+        redirect_to signup_path
+      end
+    else
+      redirect_to signup_path
+    end 
+  end
+
+  def logout
+    session[:user_id]= nil
+
+    redirect_to login_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :password_digest)
+  end
+  def prevent_login_signup
+    if session[:user_id]
+      redirect_to home_path
+    end
+  end
+
+  # def mandatory_login
+    # if session[:user_id] == nil
+      # redirect_to login_path
+    # end
+  # end
+end
